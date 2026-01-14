fa9b981d6883252477ac890f0073b7614583e60a
Antas Sinha <antassinha007@gmail.com>
2026-01-14 15:08:39 +0530

    Update TwilioConfig.java

--- src/main/java/TwilioConfig.java
+++ src/main/java/TwilioConfig.java
@@ -1,7 +1,7 @@
 public class TwilioConfig {
 
-  public static String TwilioSid = "AC9787988323bf2c6ed9a58522da91779a";
-  public static String authToken = "f1c87c8e99920cb29f6cc0483429ac1b";
-  public static String twilioFrom = "whatsapp:+14155238886";
-  public static String twilioTo = "whatsapp:+919456609191";
+  public static String TwilioSid = "";
+  public static String authToken = "";
+  public static String twilioFrom = "whatsapp:+";
+  public static String twilioTo = "whatsapp:+";
 }
